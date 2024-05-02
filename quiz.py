07b05e4752ef5e0da19e71a5a9c653ae2aea4747
Heeeeeeeeeeen <livetth@wsc.school.nz>
2024-05-02 12:01:58 +1200

    Made version 1 of the quiz

---
+++ quiz.py
@@ -0,0 +1,9 @@
+#Ask name and save it
+name=input("what is your name?")
+#greet the user to the quiz
+print("hello" ,name, "welcome to this quiz lets get started!")
+#ask question
+input("where is the eifell tower?")
+#tell user awnser 
+print("the awnser is france!")
+#end quiz
