9176a2d71bcb94ba899f9acab77d5d4b23b22f18
Heeeeeeeeeeen <livetth@wsc.school.nz>
2024-05-03 12:28:50 +1200

    quiz V2

--- quiz.py
+++ quiz.py
@@ -1,3 +1,5 @@
+#score
+score = 0 
 #Ask name and save it
 name=input("what is your name?")
 #greet the user to the quiz
@@ -7,6 +9,10 @@ awnser=input("where is the eifell tower?")
 #check if awnser is correct then give feedback
 if awnser == "france":
   print("correct you aren't an idiot")
+  score += 5
+  print("Your score is now",score)
+elif awnser == "":
+  print("I'm not gonna give you the awnser give me an actual awnser")
 else:
   print("no you idiot! its france!")
 #end quiz
