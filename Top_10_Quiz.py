bf5ca2ad51215b208e7fb0eba71fba76792300c0
Heeeeeeeeeeen <livetth@wsc.school.nz>
2024-05-28 12:23:51 +1200

    V6

--- Top_10_Quiz.py
+++ Top_10_Quiz.py
@@ -1,3 +1,5 @@
+guesses =[]
+MOST_DOWNLOADED_GAMES_AWNSERS = ["subway surfers", "stumble guys", "roblox", "condy crush saga", "race master 3d", "8 ball pool", "fifa mobile", "merge and fight", "garena free fire", "bridge race"]
 import random
 
 score = 0
@@ -28,8 +30,48 @@ def getNunber():
 number = getNunber()
 print(number)
 
-# ----- MAIN CODE ----- 
+def getLives():
+    while True:
+        lives = input("how many cances do you want?")
+        try:
+            lives = int(lives)
+            if lives >= 0:
+                return lives
+            else:
+                print("please choose a positive number")
+        except:
+            print("that wasn't a number")
 
+def isCorrect(answer, list):
+    if awnser in list:
+        return True
+    else:
+        return False
+    
+if inList(answer, MOST_DOWNLOADED_GAMES_AWNSERS):
+    if inList(answer,guesses):
+        print("you have already guessed that")
+    else:
+        print("correct")
+        score += 5
+        guesses.append(answer)
+
+# ----- MAIN CODE -----
+lives = getLives()
+score = 0
 intro()
 
-tries = getNunber()
+
+while lives > 0:
+    awnser = input("what is one of the top 10 most popular games in 2023\n").lower() 
+
+    if isCorrect(awnser, MOST_DOWNLOADED_GAMES_AWNSERS) == True:
+        print("Correct!")
+        score +=5
+    else:
+        print("wrong")
+        lives -= 1
+
+print("game Over. your final score was {}".format(score))
+
+
