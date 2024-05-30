7ea2e8098eb2f94d036ded8cb299f293d3aa6094
Heeeeeeeeeeen <livetth@wsc.school.nz>
2024-05-30 12:30:38 +1200

    Copy pasta bank

---
+++ copy_pasta.py
@@ -0,0 +1,13 @@
+COPY_PASTA_OPTIONS = ["brain dead boots top 10"]
+
+awnser = input("name a copy pasta")
+
+for i in range(len(COPY_PASTA_OPTIONS)):
+    try:   
+        awnser == COPY_PASTA_OPTIONS [i]
+        print("Today we're gonna be counting down the top 10 numbers of all time, starting it off at number 10 it's number 7 and at number 9 it's none other than number 8 now speaking of number 8 number 8 is number 6 and at number 7 it is of course number 9 but 9 in 7 and 7 in 10 need to look out because it's number 6 with number 5 quick recap because number 9 was 8 which you'll need to know for the context of this next number because it's number 5 for number 4 and in number 4 it's number 2 but number 3 is also number 5 that's right folks ladies and gentlemen we have 2 number 5s at number 6 and number 3, 3 must suck ass because it couldn't even make it into the top 10 and at number 2 it's number 1 so close number 1 but there can only be 1 number 1 at number 1 is number 5!")
+    except:
+        input("name a different copy pasta")    
+    
+
+ 
